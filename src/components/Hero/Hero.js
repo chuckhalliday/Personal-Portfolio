16ed5a4f010324bb17689bba27999adddee7d043
Charles Clark <94728513+chuckhalliday@users.noreply.github.com>
2023-11-15 09:18:48 -0800

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
         Important Disclaimer:
       </SectionSubTitle>
       <SectionText>
-        I know senior.. elected.. government officials, who I can force my political bias and.. influence on fellow.... politicians.. to vote MY way.. For a payoff, of course.
+        I know senior.. elected.. government officials, who I can force my political bias and.. influence, on fellow.... politicians.. <br /> To vote MY way. For a payoff, of course..
       </SectionText>
       <Button onClick={() => window.open("https://isntthatcorrupt.com")}>
       <ZoomImage src='../images/Breen.png' alt="Button Image"/> 
