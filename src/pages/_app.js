d33ccd8c708195ea8a7cc176490433c09e4e96d4
chuckhalliday <charleshallidayclarkii@gmail.com>
2024-10-20 01:48:54 -0700

    Require interaction

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Theme, Container, VideoStyled } from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
   const [videoEnded, setVideoEnded] = useState(false);
+  const videoRef = useRef(null);
 
   const handleVideoEnd = () => {
     setVideoEnded(true);
   };
+
+  const handleVideoPlay = () => {
+    // Play both video and audio together
+    videoRef.current.play();
+  };
+
+  const handleVideoPause = () => {
+    // Pause both video and audio together
+    videoRef.current.pause();
+  };
+
   return (
     <>
       {!videoEnded ? (
         <Container>
-        <VideoStyled onEnded={handleVideoEnd} autoPlay muted>
-          <source src="../videos/AOL (Sign On - Dial Up).mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </VideoStyled>
+          <VideoStyled
+            ref={videoRef}
+            onEnded={handleVideoEnd}
+            onClick={handleVideoPlay}
+            onPause={handleVideoPause}
+            autoPlay
+            muted={false}
+          >
+            <source src="../videos/AOL (Sign On - Dial Up).mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </VideoStyled>
+          
         </Container>
       ) : (
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
+        <Theme>
+          <Component {...pageProps} />
+        </Theme>
       )}
-      
     </>
   );
 }
- 
